3d889100e451eab12a3a24987b229a44c33c0c71
Andres-Felipe-Calderon <afelipecalderon@unicesar.edu.co>
2024-11-06 11:32:56 -0500

    configuraciones para produccion

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // Asegúrate de que los archivos se generen en esta ruta
+    },
 });
